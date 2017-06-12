1087d87b13c0df709b11c3321bbbef118c3a601a
Mathias Buus <mathiasbuus@gmail.com>
2017-06-12 09:54:16 +0200

    add crypto_shorthash tests

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ module.exports = function (sodium) {
   require('./randombytes_buf')(sodium)
   require('./crypto_sign')(sodium)
   require('./crypto_generichash')(sodium)
+  require('./crypto_shorthash')(sodium)
 }
