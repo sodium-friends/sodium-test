5cac0c80a87283c7c072f675efc59022461e4a1f
Emil Bay <github@tixz.dk>
2017-06-19 22:18:17 +0200

    forgot kdf

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 module.exports = function (sodium) {
   require('./randombytes_buf')(sodium)
   require('./crypto_sign')(sodium)
+  require('./crypto_kdf')(sodium)
   require('./crypto_generichash')(sodium)
   require('./crypto_shorthash')(sodium)
 }
