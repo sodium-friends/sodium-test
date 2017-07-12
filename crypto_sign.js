ece3fb807a5b7484dabac354cd46791450258fb2
Luke Burns <luke@lukeburns.com>
2017-07-12 15:58:31 -0400

    added test for crypto_sign_open

--- crypto_sign.js
+++ crypto_sign.js
@@ -4,6 +4,24 @@ var equals = require('buffer-equals')
 var from = require('buffer-from')
 
 module.exports = function (sodium) {
+  test('crypto_sign_open fixtures', function (assert) {
+    var fixtures = require('./fixtures/crypto_sign.json')
+
+    for (var i = 0; i < fixtures.length; i++) {
+      var publicKey = from(fixtures[i][1])
+      var message = from(fixtures[i][3])
+      var signed = from([].concat(fixtures[i][2], fixtures[i][3]))
+
+      if (!sodium.crypto_sign_open(signed, message, publicKey)) {
+        assert.fail('Failed on fixture #' + i)
+        assert.end()
+        return
+      }
+    }
+
+    assert.pass('Passed all fixtures')
+    assert.end()
+  })
   test('crypto_sign fixtures', function (assert) {
     var fixtures = require('./fixtures/crypto_sign.json')
 
