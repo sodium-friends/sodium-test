193d770f49ae9683a4d0939863ea12629a43b451
Luke Burns <luke@lukeburns.com>
2017-07-12 16:38:35 -0400

    added tests for crypto_sign_verify_detached and crypto_sign_detached

--- crypto_sign.js
+++ crypto_sign.js
@@ -45,4 +45,44 @@ module.exports = function (sodium) {
     assert.pass('Passed all fixtures')
     assert.end()
   })
+  test('crypto_sign_verify_detached fixtures', function (assert) {
+    var fixtures = require('./fixtures/crypto_sign.json')
+
+    for (var i = 0; i < fixtures.length; i++) {
+      var publicKey = from(fixtures[i][1])
+      var message = from(fixtures[i][3])
+      var signature = from(fixtures[i][2])
+
+      if (!sodium.crypto_sign_verify_detached(signature, message, publicKey)) {
+        assert.fail('Failed on fixture #' + i)
+        assert.end()
+        return
+      }
+    }
+
+    assert.pass('Passed all fixtures')
+    assert.end()
+  })
+  test('crypto_sign_detached fixtures', function (assert) {
+    var fixtures = require('./fixtures/crypto_sign.json')
+
+    for (var i = 0; i < fixtures.length; i++) {
+      var secretKey = from([].concat(fixtures[i][0], fixtures[i][1]))
+      var message = from(fixtures[i][3])
+
+      var expected = from(fixtures[i][2])
+      var actual = alloc(sodium.crypto_sign_BYTES)
+
+      sodium.crypto_sign_detached(actual, message, secretKey)
+
+      if (equals(actual, expected) === false) {
+        assert.fail('Failed on fixture #' + i)
+        assert.end()
+        return
+      }
+    }
+
+    assert.pass('Passed all fixtures')
+    assert.end()
+  })
 }
