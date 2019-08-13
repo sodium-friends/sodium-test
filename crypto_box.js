08a40bc992b96ed9d7e65f2922264467490c7427
Fedor Indutny <fedor@indutny.com>
2019-08-13 07:58:40 -0700

    crypto_box: add seal/seal_open tests

--- crypto_box.js
+++ crypto_box.js
@@ -1,5 +1,6 @@
 var tape = require('tape')
 var alloc = require('buffer-alloc')
+var toBuffer = require('buffer-from')
 
 module.exports = function (sodium) {
   tape('crypto_box_keypair generate key-pair', function (t) {
@@ -19,6 +20,39 @@ module.exports = function (sodium) {
     t.notEqual(pubKey, pubKeyCopy)
     t.notEqual(secret, secretCopy)
 
+    t.end()
+  })
+
+  tape('crypto_box_seal/crypto_box_seal_open self-decrypt', function (t) {
+    var pubKey = alloc(sodium.crypto_box_PUBLICKEYBYTES)
+    var secret = alloc(sodium.crypto_box_SECRETKEYBYTES)
+
+    sodium.crypto_box_keypair(pubKey, secret)
+
+    var msg = toBuffer('hello world')
+    var cipher = alloc(sodium.crypto_box_SEALBYTES + msg.length)
+    sodium.crypto_box_seal(cipher, msg, pubKey)
+
+    var out = alloc(cipher.length - sodium.crypto_box_SEALBYTES)
+    sodium.crypto_box_seal_open(out, cipher, pubKey, secret)
+    t.same(out.toString(), msg.toString())
+    t.end()
+  })
+
+  tape('crypto_box_seal_open cross-decrypt', function (t) {
+    var pubKey = toBuffer(
+      'e0bb844ae3f48bb04323c8dfe7c34cf86608db2e2112f927953060c80506287f', 'hex')
+    var secret = toBuffer(
+      '036a9de1ecc9d152cf39fed1b3e15bf761ae39a299031adc011cc9809041abfa', 'hex')
+    var cipher = toBuffer(
+      '249912e916ad8bcf96a3f9b750da2703' +
+      '2eccdf83b5cff0d6a59a8bbe0bcd5823' +
+      '5de9fbca55bd5416c754e5e0e0fe2f0c' +
+      '4e50df0cb302f1c4378f80', 'hex')
+
+    var out = alloc(cipher.length - sodium.crypto_box_SEALBYTES)
+    sodium.crypto_box_seal_open(out, cipher, pubKey, secret)
+    t.same(out.toString(), 'hello world')
     t.end()
   })
 }
