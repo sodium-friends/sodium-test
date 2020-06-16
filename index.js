ad5b9658e47d421e3c7a16f9add14528d12344d2
Christophe Diederichs <chris.d.itunes@gmail.com>
2020-06-16 16:10:18 +0200

    crypto_box_seed_keypair tests added

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 module.exports = function (sodium) {
   require('./randombytes_buf')(sodium)
   require('./crypto_sign')(sodium)
+  require('./crypto_box')(sodium)
+  require('./crypto_kx')(sodium)
   require('./crypto_kdf')(sodium)
   require('./crypto_generichash')(sodium)
   require('./crypto_shorthash')(sodium)
