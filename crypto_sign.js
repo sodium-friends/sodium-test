1728b7bb4e56fed4154057c56b7aacb0ac1babd4
Christian Bundy <christianbundy@fraction.io>
2020-09-03 09:22:30 -0700

    Add ed25519 conversion tests
    This adds basic tests for crypto_sign_ed25519_pk_to_curve25519(), using
    the Sodium-Native implementation as a source of truth. This diverges
    from the upstream ed25519_convert.c test in a few small ways:
    
    - Conversion failures throw an error rather than return `-1` or `false`.
    - Some methods return `undefined`, so we don't check their return value.
    - Most exports are changed from crypto_sign_ed25519 to crypto_sign.

--- crypto_sign.js
+++ crypto_sign.js
@@ -164,6 +164,69 @@ module.exports = function (sodium) {
 
     assert.end()
   })
+
+  test('ed25519 convert', (assert) => {
+    const keypair_seed = new Uint8Array([
+      0x42, 0x11, 0x51, 0xa4, 0x59, 0xfa, 0xea, 0xde, 0x3d, 0x24, 0x71,
+      0x15, 0xf9, 0x4a, 0xed, 0xae, 0x42, 0x31, 0x81, 0x24, 0x09, 0x5a,
+      0xfa, 0xbe, 0x4d, 0x14, 0x51, 0xa5, 0x59, 0xfa, 0xed, 0xee
+    ])
+
+    const ed25519_pk = new Uint8Array(sodium.crypto_sign_PUBLICKEYBYTES)
+    const ed25519_skpk = new Uint8Array(sodium.crypto_sign_SECRETKEYBYTES)
+    const curve25519_pk = new Uint8Array(sodium.crypto_scalarmult_BYTES)
+    const curve25519_pk2 = new Uint8Array(sodium.crypto_scalarmult_BYTES)
+    const curve25519_sk = new Uint8Array(sodium.crypto_scalarmult_BYTES)
+    const curve25519_pk_hex = new Uint8Array(sodium.crypto_scalarmult_BYTES * 2 + 1)
+    const curve25519_sk_hex = new Uint8Array(sodium.crypto_scalarmult_BYTES * 2 + 1)
+
+    assert.ok(sodium.crypto_sign_SEEDBYTES <= sodium.crypto_hash_sha512_BYTES)
+
+    sodium.crypto_sign_seed_keypair(ed25519_pk, ed25519_skpk, keypair_seed)
+    sodium.crypto_sign_ed25519_pk_to_curve25519(curve25519_pk, ed25519_pk)
+    sodium.crypto_sign_ed25519_sk_to_curve25519(curve25519_sk, ed25519_skpk)
+
+    const expected_pk = [
+      0xf1, 0x81, 0x4f, 0x0e, 0x8f, 0xf1, 0x04, 0x3d, 0x8a, 0x44, 0xd2, 0x5b,
+      0xab, 0xff, 0x3c, 0xed, 0xca, 0xe6, 0xc2, 0x2c, 0x3e, 0xda, 0xa4, 0x8f,
+      0x85, 0x7a, 0xe7, 0x0d, 0xe2, 0xba, 0xae, 0x50
+    ]
+    const expected_sk = [
+      0x80, 0x52, 0x03, 0x03, 0x76, 0xd4, 0x71, 0x12, 0xbe, 0x7f, 0x73, 0xed,
+      0x7a, 0x01, 0x92, 0x93, 0xdd, 0x12, 0xad, 0x91, 0x0b, 0x65, 0x44, 0x55,
+      0x79, 0x8b, 0x46, 0x67, 0xd7, 0x3d, 0xe1, 0x66
+    ]
+
+    assert.deepEqual(curve25519_pk, expected_pk)
+    assert.deepEqual(curve25519_sk, expected_sk)
+
+    for (let i = 0; i < 500; i++) {
+      sodium.crypto_sign_keypair(ed25519_pk, ed25519_skpk)
+      sodium.crypto_sign_ed25519_pk_to_curve25519(curve25519_pk, ed25519_pk)
+
+      sodium.crypto_sign_ed25519_sk_to_curve25519(curve25519_sk, ed25519_skpk)
+      sodium.crypto_scalarmult_base(curve25519_pk2, curve25519_sk)
+      assert.deepEqual(curve25519_pk, curve25519_pk2)
+    }
+
+    // Upstream tests return `-1`, should these throw or return `false`?
+    ed25519_pk.set(new Uint8Array(ed25519_pk.length))
+    assert.throws(() => {
+      sodium.crypto_sign_ed25519_pk_to_curve25519(curve25519_pk, ed25519_pk)
+    })
+
+    assert.throws(() => {
+      ed25519_pk[0] = 2
+      sodium.crypto_sign_ed25519_pk_to_curve25519(curve25519_pk, ed25519_pk)
+    })
+
+    assert.throws(() => {
+      ed25519_pk[0] = 5
+      sodium.crypto_sign_ed25519_pk_to_curve25519(curve25519_pk, ed25519_pk)
+    })
+
+    assert.end()
+  })
 }
 
 function parseTest (t) {
